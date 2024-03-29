6d96d92e173bc39bae293aa0dc817267e11a4a1f
Nathan Schrope <NathanSchrope@outlook.com>
2024-03-28 22:14:24 -0400

    general renaming and adding a new project for a health API

---
+++ HealthAPI/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HealthAPI.Controllers
+{
+    [ApiController]
+    public class HealthController : Controller
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "health")]
+        public string Get()
+        {
+            return "healthy";
+        }
+    }
+}
